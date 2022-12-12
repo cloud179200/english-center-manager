25e77e86728e999b897519b03bfb940b6c3813ec
anhqb2000 <anhleqb2000@gmail.com>
2022-12-12 13:33:43 +0700

    link ngrok

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   defaultPath: "/",
-  HOST_API: "https://5c39-222-252-30-68.ap.ngrok.io" || process.env.HOST_API,
+  HOST_API: "https://0f8f-222-252-30-68.ap.ngrok.io" || process.env.HOST_API,
 };
 
 export default config;

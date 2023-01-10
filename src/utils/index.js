0cf1b682d4aa22fedf8d746e2ab4393e94dae2a5
anhqb2000 <anhleqb2000@gmail.com>
2023-01-11 02:15:23 +0700

    Fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
 
-export const getLocalStorageUserinfo = async () => {
-  let userInfo = null;
-  if (localStorage.getItem("persist:root")) {
-    const jsonParse = await JSON.parse(localStorage.getItem("persist:root"));
-    if (jsonParse?.user) {
-      const userParse = await JSON.parse(jsonParse.user);
-      userInfo = userParse.userInfo;
-    }
-  }
-  return userInfo;
-};
-
 export const sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
